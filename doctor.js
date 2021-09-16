c167985c489855a98306d5a48f6fc21e6a3b9bea
Abdur Rahman tumzied <50454992+tz01x@users.noreply.github.com>
2021-09-16 17:39:15 +0600

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -2,82 +2,162 @@
 
 export function Doctor() {
 
-    const doctors = [{
-        doctorID: 0,
-        team: '',
-        teamID: 0,
-        doctorTypes: 'consultant',
-        firstName: '',
-        lastName: '',
-        email: "",
-        status: 'active',
-
-    }];
-    let lastDoctorID = 0;
-    let lastTeamID = 0;
-
-    const teamMates = [
+    const doctors = [
         {
-            teamID: 0,
-            team: '',
-            members: [],//members list only store doctorID
-
-        }
+            doctorID: "6215",
+            firstName: "Jalaluddin",
+            lastName: "Mahbub",
+            teamID: "008",
+            doctorType: "Consultant",
+            email: "jabub@hospital.com",
+            active: true,
+            doctorRequests: []
+        },
+        {
+            doctorID: "6216",
+            firstName: "Amin",
+            lastName: "Morshed",
+            teamID: "008",
+            doctorType: "Assistant Consultant",
+            email: "amhed@hospital.com",
+            active: true
+        },
+        {
+            doctorID: "6214",
+            firstName: "Mahady",
+            lastName: "Selim",
+            teamID: "005",
+            doctorType: "Consultant",
+            email: "malim@hospital.com",
+            active: true,
+            doctorRequests: ["6213", ]
+        },
+        {
+            doctorID: "6213",
+            firstName: "Jamela",
+            lastName: "Begum",
+            teamID: "005",
+            doctorType: "RMO",
+            email: "jagum@hospital.com",
+            active: false
+        },
+    
     ];
 
-    const doctorRequests = [];
+    const team = [
+        {
+            teamName: "nephrology",
+            teamID: "008",
+            consultantInCharge: "6215",
+            teamMates: ["6216",]
+        },
+        {
+            teamName: "cardiology",
+            teamID: "005",
+            consultantInCharge: "6214",
+            teamMates: []
+        },
+    ]
+
+    let lastDoctorID = -1;
+   
+
 
 
 
     /**
      * @param {string} firstName
      * @param {string} lastName
-     * @param {string} doctorTypes
+     * @param {string} doctorType
      * @returns {number} doctorID:  of newdly added doctor to doctorRequests list 
      */
-    this.addDoctor = (firstName, lastName, doctorTypes) => {
+    this.addDoctor = (firstName, lastName, doctorType) => {
+        // get last max id from the doctor 
+        if(lastDoctorID===-1){
+            let maxID=0;
+            doctors.map((d=>{
+
+                maxID<parseInt(d.doctorID)?
+                    maxID=parseInt(d.doctorID):null;
+                
+                
+            }));
+            lastDoctorID=maxID;
+           
+        }
+        
 
 
-        let email = firstName.slice(0, 2) + lastName.slice(-4, -1) + "@doc.com";
-        let doctorID = lastDoctorID + 1;
+        let email = firstName.slice(0, 2) + lastName.slice(-4, -1) + "@hospital.com";
+        
+        let doctorID = (new Array(4).join('0') + (lastDoctorID + 1)).substr(-4); //substring from last 4 digit 
         const doc = {
             doctorID,
             firstName,
             lastName,
-            doctorTypes,
             email,
-            status: 'deactive',
+            active: false,
+            teamID:'',
+            doctorType,
         }
+        doctorType==='Consultant'?doc['doctorRequests']=[]:null;
 
         lastDoctorID += 1;
 
-        doctorRequests.push(doc);
+        doctors.push(doc);
         return doctorID;
 
     }
 
     /**
      * @param {number} doctorID : doctorID
-     * @param {string} team : team name
+     * @param {string} teamID : team name
+     * @param {object} currentActingDoctor : team name
      */
-    this.addTeam = (doctorID, team) => {
+    this.addTeam = (doctorID, teamID,currentActingDoctor) => {
 
+        if(currentActingDoctor.doctorType!=='Consultant'){return;}
+        
+        let idx = doctors.findIndex((doc) => doc.doctorID === doctorID);
+        let ConsultantDocIdx = doctors.findIndex((doc) => doc.doctorID === currentActingDoctor.doctorID);
+        if (ConsultantDocIdx!==-1 && idx !== -1) {
 
-        let idx = doctorRequests.findIndex((rdoc) => rdoc.doctorID === doctorID);
-        if (idx != -1) {
-            doctorRequests[idx] = { ...doctorRequests[idx], team };
+            doctors[idx] = { ...doctors[idx], teamID };
+
+            // adding the new doctor to Consultant Doc `doctorRequests` field 
+            doctors[ConsultantDocIdx].doctorRequests=[...doctors[ConsultantDocIdx].doctorRequests,doctors[idx].doctorID];
+
         }
 
     }
     this.listOfDoctor = () => [...doctors];
     // this.listOfDoctorRequests=()=>[...doctorRequests];
+
+    this.listOfTeams = () => [...team];
+
     
 
-    /**@returns {Array} DoctorRequested list*/
-    this.promoteNewDoctor = () => [...doctorRequests];
+    /**
+     * @param {Object}: 
+     * @returns {Array}: DoctorRequested list
+     * 
+    */
+    this.promoteNewDoctor = (currentActingDoctor) => {
+        if(currentActingDoctor.doctorType!='Assistant Consultant'){return [];}
+        let newDocID=[];
+        
+        doctors.map((doc)=>{
+            doc.doctorType=='Consultant'? newDocID=[...newDocID,...doc.doctorRequests]:null;
+        });
 
-    /**@returns {Array} teamMates list */
-    this.listOfteamMates = () => [...teamMates];
+        return doctors.filter((doc)=>{
+            let f=newDocID.findIndex(docidx=> doc.doctorID===docidx);
+            return f!=-1?true:false
+        });
+
+    };
+
+
 
     /**
      * @param {string} fullname: fullname should be separated by space. forexample, 'Amin Morshed' Amin is lastName and Morshed is firstName
@@ -93,15 +173,15 @@ export function Doctor() {
 
         // disable doctor account.
 
-        doctors[docIdx] = { ...doctors[docIdx], status: 'deactive' }
+        doctors[docIdx] = { ...doctors[docIdx], active:false }
 
-        let teamID = doctors[docIdx].teamID;
+   
 
-        let teamMateidx = teamMates.findIndex(t => t.teamID === teamID);
+        let teamIDX = team.findIndex(t => t.teamID === doctors[docIdx].teamID);
 
         // Remove doctor from the team,
 
-        teamMates[teamMateidx] = { ...teamMates[teamMateidx], members: teamMates[teamMateidx].members.filter(docID => docID != doctors[docIdx].doctorID) };
+        team[teamIDX] = { ...team[teamIDX], teamMates: team[teamIDX].teamMates.filter(docID => docID != doctors[docIdx].doctorID) };
 
     }
 
@@ -111,52 +191,58 @@ export function Doctor() {
      * @returns 
      */
 
-    this.approveDoctor = (doctorID) => {
+    this.approveDoctor = (doctorID,currentActingDoctor) => {
 
-        let doctorRequestsIDX = doctorRequests.findIndex(doc => doc.doctorID === doctorID);
+        if(currentActingDoctor.doctorType!=='Consultant'){return;}
 
-        if (doctorRequestsIDX == -1) {
+        let doctorIDX = doctors.findIndex(doc => doc.doctorID === doctorID);
+
+        if (doctorIDX === -1) {
             return;
         }
 
-        let doctor = doctorRequests[doctorRequestsIDX];
-        if (doctor == null || doctor == undefined) {
-            return;
-        }
-        // remove index item from doctorRequests
+        // remove doctor from item from doctorRequests
+        doctors.find((doc)=>{
+            if(doc.doctorType==='Consultant'){
+                let idx=doc.doctorRequests.findIndex(docID=> docID===doctors[doctorIDX].doctorID);
 
-        doctorRequests.splice(doctorRequestsIDX, 1);
+                if(idx!==-1){
+                
+                    doc.doctorRequests=[...doc.doctorRequests.filter((_,i)=>i!==idx)];
+                    return true;
+                }
+
+            }
+            return false
+        });
+      
+    
+        
+
+        
 
 
         // search for team 
-        let teamMateIdx = teamMates.findIndex(t => t.team === doctor.team);
-        // if there is no team create team 
-        if (teamMateIdx == -1) {
+        let teamIDX = team.findIndex(t => t.teamID === doctors[doctorIDX].teamID);
 
-            teamMates.push({ team: doctor.team, members: [doctor.doctorID], teamID: lastTeamID + 1 });
+        // checking current acting doctor is a consultantInCharge for the team 
+        if (teamIDX !== -1 && team[teamIDX].consultantInCharge==currentActingDoctor.doctorID) {
 
-            lastTeamID += 1;
-        } else {
 
-            // add to doctorID to team.member list 
+                // update doctor status to active 
+                doctors[doctorIDX]={...doctors[doctorIDX],active:true};
 
-            teamMates[teamMateIdx] = { ...teamMates[teamMateIdx], members: [...teamMates[teamMateIdx].members, doctor.doctorID] };
-
+                // add to team 
+                team[teamIDX]={
+                    ...team[teamIDX],
+                    teamMates:[
+                        ...team[teamIDX].teamMates,
+                        doctors[doctorIDX].doctorID
+                    ]
+                };
+            
+                
         }
-
-
-
-        // update doctor status to active 
-
-        doctor.teamID = lastTeamID;
-
-        //  add Doc to doctors list 
-
-        doctors.push(doctor);
-
-
-
-
 
     }
 
@@ -169,47 +255,54 @@ export function Doctor() {
 console.log('**************doctor part*******************');
 const doctor = new Doctor();
 
-console.log(doctor.listOfDoctor());
+let ConsultantDoctor=doctor.listOfDoctor()[0];
+let AssistantConsultant=doctor.listOfDoctor()[1];
 // console.log(doctor.promoteNewDoctor());
 
-
+console.log(' Adding Amin Morshed  ');
 // Add a new doctor with the doctorType `RMO`
 let doctorID = doctor.addDoctor('Morshed', 'Amin', 'RMO');
 
 // the new doctor team `008`
-doctor.addTeam(doctorID, '008');
+doctor.addTeam(doctorID, '008',ConsultantDoctor);
 
+console.log('Promote the new doctor  ');
 //  Promote the new doctor  
-console.log(doctor.promoteNewDoctor());
+console.log(doctor.promoteNewDoctor(AssistantConsultant));
 
 // if consultantInCharge approve requested Doctor 
 // then
-doctor.approveDoctor(doctorID);
-
+doctor.approveDoctor(doctorID,ConsultantDoctor);
+console.log('After approve ');
 // displaying result
-console.log('doctors list');
+console.log('Doctors list');
 console.log(doctor.listOfDoctor());
 
 console.log('recruited doctors list');
-console.log(doctor.promoteNewDoctor());
+console.log(doctor.promoteNewDoctor(AssistantConsultant));
 
-console.log('temsMembers list');
-console.log(doctor.listOfteamMates());
+console.log('Team list');
+console.log(doctor.listOfTeams());
 
+
+console.log(' Remove Amin Morshed from the team ');
 // Remove Amin Morshed from the team, and disable his account
 doctor.removeDoctorFromTeam('Amin Morshed');
 
 
 
 // displaying result
-console.log('doctors list');
+console.log('Doctors list');
 console.log(doctor.listOfDoctor());
 
-console.log('recruited doctors list');
-console.log(doctor.promoteNewDoctor());
+console.log('Team list');
+console.log(doctor.listOfTeams());
 
-console.log('temsMembers list');
-console.log(doctor.listOfteamMates());
+
+console.log('recruited doctors list');
+console.log(doctor.promoteNewDoctor(AssistantConsultant));
+
+
 console.log('**************doctor part end*******************');
 
 
